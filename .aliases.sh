14846abb9b6c6c5f6d45de83fda12c9032ca858b
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-06-14 20:50:32 -0500

    Adds aliases to dotfiles

---
+++ .aliases.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+alias c='clear'
+alias v='vim'
+alias vi='vim'
+alias e='exit'
+alias l='exa -l'
+alias ls='exa -l'
+alias lh='exa -l -a'
+alias reload='unalias -a && . "$HOME"/.aliases.sh'
