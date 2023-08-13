bedace63d4041f67d291e570165d13f4246492a7
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-08-13 17:51:20 -0400

    More neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,12 +9,11 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
 -- Useful vanilla keybindings
-vim.api.nvim_set_keymap('n', 'q', ':q<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>w', '<C-w><C-w>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>j', ':bnext<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>k', ':bprev<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>f', 'za', { noremap = true, silent = true })
-
+vim.api.nvim_set_keymap("n", "q", ":q<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>w", "<C-w><C-w>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>j", ":bnext<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>k", ":bprev<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>f", "za", { noremap = true, silent = true })
 
 -- Make sure that vim-closetag works on ERB files. We have to set this
 -- global variable *before* we configure our plugin manager; otherwise,
@@ -25,108 +24,151 @@ vim.g.closetag_filetypes = "eruby,template"
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Install Plugins
 require("lazy").setup({
-  -- Manage language server installations
-  {
-    "williamboman/mason.nvim",
-    opts = {},
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = {"pyright", "gopls"},
-      handlers = { 
-        function(server_name) 
-          require("lspconfig")[server_name].setup({})
-        end 
-      },
-    },
-  },
-  "neovim/nvim-lspconfig",
-  -- Manage comments
-  {
-    'numToStr/Comment.nvim',
-    opts = {}
-  },
-  -- Manage auto pairs
-  {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    opts = {}
-  },
-  -- Manage snippets
-  {
-    'dcampos/nvim-snippy',
-    opts = {
-      mappings = {
-        is = {
-          ['<Tab>'] = 'expand_or_advance',
-          ['<S-Tab>'] = 'previous',
-        },
-      },
-    },
-  },
-  'dcampos/cmp-snippy',
-  -- Manage autocomplete
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path"
-    },
-    config = function()
-      local cmp = require("cmp")
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            require('snippy').expand_snippet(args.body)
-          end,
-        },
-        completion = {
-          autocomplete = false,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-n>'] = cmp.mapping(function() 
-              if cmp.visible() then
-                cmp.select_next_item()
-              else
-                cmp.complete()
-              end
-          end),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<C-space>'] = cmp.mapping.complete(),
-          ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), {'i','c'}),
-          -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'snippy' },
-          { name = "path" },
-          { name= 'buffer' },
-        })
-      })
-    end
-  },
-  {
-    'projekt0n/github-nvim-theme',
-    config = function ()
-      vim.cmd.colorscheme("github_light_high_contrast")
-    end
-  },
+	-- Manage language server installations
+	{
+		"williamboman/mason.nvim",
+		opts = {},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = { "pyright", "gopls", "svelte" },
+			handlers = {
+				function(server_name)
+					require("lspconfig")[server_name].setup({})
+				end,
+			},
+		},
+	},
+	"neovim/nvim-lspconfig",
+	-- Manage comments
+	{
+		"numToStr/Comment.nvim",
+		opts = {},
+	},
+	-- Manage auto pairs
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {},
+	},
+	-- Manage snippets
+	{
+		"dcampos/nvim-snippy",
+		opts = {
+			mappings = {
+				is = {
+					["<Tab>"] = "expand_or_advance",
+					["<S-Tab>"] = "previous",
+				},
+			},
+		},
+	},
+	"dcampos/cmp-snippy",
+	-- Manage autocomplete
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+		},
+		config = function()
+			local cmp = require("cmp")
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("snippy").expand_snippet(args.body)
+					end,
+				},
+				completion = {
+					autocomplete = false,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-n>"] = cmp.mapping(function()
+						if cmp.visible() then
+							cmp.select_next_item()
+						else
+							cmp.complete()
+						end
+					end),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<C-space>"] = cmp.mapping.complete(),
+					["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+					-- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "snippy" },
+					{ name = "path" },
+					{ name = "buffer" },
+				}),
+			})
+		end,
+	},
+	-- Manage colorscheme
+	{
+		"NLKNguyen/papercolor-theme",
+		config = function()
+			vim.opt.background = "light"
+			vim.cmd.colorscheme("papercolor")
+		end,
+	},
+	-- Manage Fuzzy Finding
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			vim.api.nvim_set_keymap("n", "<leader>sf", ":Telescope find_files<CR>", { noremap = true, silent = true })
+			vim.api.nvim_set_keymap("n", "<leader>sg", ":Telescope live_grep<CR>", { noremap = true, silent = true })
+			vim.api.nvim_set_keymap("n", "<leader>sb", ":Telescope buffers<CR>", { noremap = true, silent = true })
+			vim.api.nvim_set_keymap("n", "<leader>sh", ":Telescope help_tags<CR>", { noremap = true, silent = true })
+		end,
+	},
+	-- Manage Formatting
+	{
+		"sbdchd/neoformat",
+		config = function()
+			vim.api.nvim_set_keymap("n", "<leader>p", ":Neoformat<CR>", { noremap = true, silent = true })
+		end,
+	},
+	-- Manage Syntax Highlighting
+	--
+	-- If and when indentation and syntax highlighting in React and Svelte files
+	-- works with TreeSitter, we might want to consider using that; until then,
+	-- the classic plugins will suffice.
+	--
+	-- JSX
+	{
+		"maxmellon/vim-jsx-pretty",
+		dependencies = {
+			"yuezk/vim-js",
+			"HerringtonDarkholme/yats.vim",
+		},
+	},
+	-- Svelte
+	{
+		"evanleck/vim-svelte",
+		dependencies = {
+			"pangloss/vim-javascript",
+			"othree/html5.vim",
+		},
+	},
 })
